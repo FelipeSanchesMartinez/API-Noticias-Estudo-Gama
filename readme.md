7fb7567af47c169385ccedad037484f1f2034303
Felipe Sanches <felipe_sanches94@hotmail.com>
2023-04-12 21:51:56 -0300

    subindo para o github

---
+++ readme.md
@@ -0,0 +1,5 @@
+# API de notícias - Gama Academy - XP 49
+
+API desenvolvida com a turma XP 49 da Gama Academy.
+
+O domínio da API é um blog de notícias. Foram implementadas funcionalidades CRUD para notícias e autores.
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import app from "./app.js";
+import db from "./configs/database.js";
+
+try {
+	await db.sync({ alter: true });
+	console.log("A conexão com o banco de dados foi bem sucedida!");
+
+	app.listen(3333, () => {
+		console.log("Servidor iniciado na porta 3333");
+	});
+} catch (error) {
+	console.log("Não foi possível se conectar com o banco de dados:", error);
+	process.exit(1);
+}
